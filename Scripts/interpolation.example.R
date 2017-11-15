315427c08f83b2ed1d9fc9d1b32d134d37e4d435
samuel.gerber@kitware.com <samuel.gerber@kitware.com>
2017-11-15 12:27:35 -0500

    added interpolation example

---
+++ Scripts/interpolation.example.R
@@ -0,0 +1,28 @@
+library( mop )
+source( "plots.R" )
+
+load( "../Data/Bullit01/all.trees.processed.01.Rdata" )
+
+X1 = all$data[[1]][,1:4]
+w1 = all$data[[1]]$v
+X1[, 4] = X1[, 4] * 100;
+  
+X2 = all$data[[2]][,1:4]
+w2 = all$data[[2]]$v
+X2[, 4] = X2[, 4] * 100;
+
+
+gmra1 = gmra.create.ikm(X=X1, eps=0.001, nKids=8)
+gmra2 = gmra.create.ikm(X=X2, eps=0.001, nKids=8)
+
+trp.lp <- multiscale.transport.create.lp( )
+icprop <- multiscale.transport.create.iterated.capacity.propagation.strategy( 1, 0 )
+multiscale.transport.set.propagation.strategy.1( trp.lp, icprop )
+multiscale.transport.add.expand.neighborhood.strategy(trp.lp, 1 )
+t <- system.time( trp <- multiscale.transport.solve( trp.lp, gmra1, gmra2, p = 2, nType=0, 
+                                       dType=3, w1=w1, w2=w2 ) )
+
+print(t)
+
+plot.interpolation.2d(trp, 0.01)
+
--- Scripts/plots.R
+++ Scripts/plots.R
@@ -2,13 +2,13 @@ source("read.tre.R")
 library(mop)
 
 
-plot.interpolation.2d <- function(trp1){
+plot.interpolation.2d <- function(trp1, radius.scaling ){
   index = 0
   for( t in c( seq(0, 1, by=0.01), seq(1, 0, by=-0.01) ) ){ 
     X = multiscale.transport.interpolate(trp1, length(trp1$map), t=t)
     #plot(NA, xlim=range(X$X[,1]), ylim=range(X$X[,2]), 
     #     bty="n", xlab="", ylab="", axes=FALSE)
-    symbols( X$X, circles=X$X[,4]/10, inches=FALSE, bg="#00000010", 
+    symbols( X$X, circles=X$X[,4]*radius.scaling, inches=FALSE, bg="#00000010", 
              fg="#00000000", bty="n", xlab="", ylab="", xaxt="n", yaxt="n")
     dev.copy(png, sprintf( "ip-%.5d.png", index) )
    dev.off()
@@ -19,7 +19,7 @@ plot.interpolation.2d <- function(trp1){
 
 
 
-plot.interpolation.3d <- function(trp1){
+plot.interpolation.3d <- function(trp1, radius.scaling){
 
   library(rgl)
 
@@ -29,7 +29,7 @@ plot.interpolation.3d <- function(trp1){
   for( i in 1:n ){ 
     X = multiscale.transport.interpolate(trp1, length(trp1$map), t=ts[i])
     plot3d(X$X[,1:3], alpha=0.15, box=FALSE, axes=FALSE, 
-           xlab="", ylab="", zlab="", type="s", radius=X$X[,4]/10 )
+           xlab="", ylab="", zlab="", type="s", radius=X$X[,4]*radius.scaling )
     rgl.viewpoint( 0.2 * (i-1), -90 + (i-1) * 10/n )
     rgl.snapshot( sprintf( "ip3d-%.5d.png", i) )
     index = index +1
@@ -37,5 +37,8 @@ plot.interpolation.3d <- function(trp1){
 
 }
 
+
+#imagemagick gif command
+# convert -dispose previous -delay 5 -alpha remove -loop 0 ip-00*.png ip.gif
 
 
